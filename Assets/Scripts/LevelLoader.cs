0da17a7d973daf3e917a5bf52a92da85f7351549
Eric <devel010@umn.edu>
2019-11-19 17:43:19 -0600

    First

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int waitSeconds = 3;
+    int currentScene;
+
+    public void StartRoutine() {
+        StartCoroutine(WaitToLoad());
+    }
+
+    IEnumerator WaitToLoad() {
+        yield return new WaitForSeconds(waitSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene () {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    void Start () {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentScene == 0) {
+            StartRoutine();
+        }
+    }
+}
