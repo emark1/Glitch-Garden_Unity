cf7c2bee11c48b62ec869b19cc2737b26cac4c25
Eric <devel010@umn.edu>
2019-11-25 16:53:05 -0600

    Added axe to attack animation

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] GameObject weapon, gun;
+
+    public void Fire() {
+        Instantiate(weapon, gun.transform.position, transform.rotation);
     }
 }
