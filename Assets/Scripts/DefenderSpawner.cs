9f06271e11b94d744b94bc1f75b823daf3690e4e
Eric <devel010@umn.edu>
2019-12-02 13:56:29 -0600

    Can now place defenders primitively

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] GameObject defender;
+    private void OnMouseDown() {
+        SpawnDefender(GetSquareClicked());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Vector2 GetSquareClicked() {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+    private void SpawnDefender (Vector2 position) {
+        Instantiate(defender, position, transform.rotation);
     }
 }
